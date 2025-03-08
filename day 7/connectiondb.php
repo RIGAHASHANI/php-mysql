d5f23313c5bd7d32f8d4121a6edc0557c60ec6ad
RIGAHASHANI <rigahashani11@gmail.com>
2025-03-08 07:21:37 -0800

    day 7

---
+++ day 7/connectiondb.php
@@ -0,0 +1,18 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+
+
+try{
+    $conn = new PDO("mysql:host=$host;",$user, $pass);
+
+    
+
+    echo "Connected";
+}catch(Exception $e){
+
+    echo "Not connected";
+}
+
+?>
